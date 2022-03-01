0b8c73bae6c2d3f007fe588dbbb52d6d70d521f1
serhat_derya <88051030+SerhatDerya@users.noreply.github.com>
2022-03-01 15:46:42 +0300

    Add files via upload

---
+++ 2_Introduction_to_Cpp/practise1.cpp
@@ -0,0 +1,17 @@
+/* some basic operations */
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    
+    int a=5, b=2;
+    
+    cout << "Initial Values: a=" << a << " b=" << b << endl;
+    
+    a++;
+    
+    cout << "the value of a after incrementation is =" << a << endl;
+    
+    cout << "result of (a-b)=" << a-b << endl;
+}
